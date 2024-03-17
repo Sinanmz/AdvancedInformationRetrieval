c16ccecb4a46df3483008cc309f3e89eea250fc9
Sina Namazi <sinanamazi2000@gmail.com>
2024-03-17 20:04:11 +0330

    Changed the file path of data file

--- Logic/tests/test_crawler.py
+++ Logic/tests/test_crawler.py
@@ -51,5 +51,5 @@ expected_fields = {
     "reviews": List[List[str]],
 }
 
-json_file_path = "../IMDB_Crawled.json"
+json_file_path = "data/IMDB_Crawled.json"
 check_field_types(json_file_path, expected_fields)
