3e05fad222b8c3e1a0ce75abb5f0717c7ca25497
Sina Namazi <sinanamazi2000@gmail.com>
2024-06-27 12:26:52 +0330

    Bert finetuner main completed.

--- Logic/core/finetuner/main.py
+++ Logic/core/finetuner/main.py
@@ -1,5 +1,18 @@
+import os
+import sys
+current_script_path = os.path.abspath(__file__)
+finetuner = os.path.dirname(current_script_path)
+core_dir = os.path.dirname(finetuner)
+Logic_dir = os.path.dirname(core_dir)
+project_root = os.path.dirname(Logic_dir)
+if project_root not in sys.path:
+    sys.path.append(project_root)
+
+from Logic.core.finetuner.BertFinetuner_mask import BERTFinetuner
+
+
 # Instantiate the class
-bert_finetuner = BERTFinetuner('path/to/your/file.json', top_n_genres=5)
+bert_finetuner = BERTFinetuner(project_root+'/data/IMDB_Crawled.json', top_n_genres=5)
 
 # Load the dataset
 bert_finetuner.load_dataset()
@@ -16,5 +29,9 @@ bert_finetuner.fine_tune_bert()
 # Compute metrics
 bert_finetuner.evaluate_model()
 
+bert_finetuner.classification_report('train')
+bert_finetuner.classification_report('val')
+bert_finetuner.classification_report('test')
+
 # Save the model (optional)
-bert_finetuner.save_model('Movie_Genre_Classifier')
+# bert_finetuner.save_model('Movie_Genre_Classifier')
